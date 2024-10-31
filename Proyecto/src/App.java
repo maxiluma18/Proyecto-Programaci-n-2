75fb41fb3e9dd40f251627b0c563f5a7a739884f
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2024-10-31 12:03:16 -0300

    Add Files and Junit for Tests

---
+++ Proyecto/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println(suma());
+    }
+
+    public static int suma() {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        return c;
+    }
+}
