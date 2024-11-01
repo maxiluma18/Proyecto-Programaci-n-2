2bcc04995e76e93684064e606f322d54a9bde876
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2024-11-01 15:27:08 -0300

    Add basic functions of Clientes and Aeropuertos

--- Proyecto/src/Aeropuerto.java
+++ Proyecto/src/Aeropuerto.java
@@ -1,3 +1,27 @@
-public class Aeropuerto {
+import javax.management.RuntimeErrorException;
 
+public class Aeropuerto {
+    private String nombre;
+    private String pais;
+    private String provincia;
+    private String direccion;
+
+    public Aeropuerto(String nombre, String pais, String provincia, String direccion) {
+        if (validacion(nombre, pais, provincia, direccion)) {
+            this.nombre = nombre;
+            this.pais = pais;
+            this.provincia = provincia;
+            this.direccion = direccion;
+        } else {
+            throw new RuntimeErrorException(null, "Los datos ingresados no son validos");
+        }
+    }
+
+    // agregar validacion e irep con las 4 variables
+    public boolean validacion(String nombre, String pais, String provincia, String direccion) {
+        if (nombre.isEmpty() || pais.isEmpty() || provincia.isEmpty() || direccion.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
--- Proyecto/src/Cliente.java
+++ Proyecto/src/Cliente.java
@@ -1,3 +1,25 @@
-public class Cliente {
+import javax.management.RuntimeErrorException;
 
+public class Cliente {
+    private int dni;
+    private String nombre;
+    private String telefono;
+
+    public Cliente(int dni, String nombre, String telefono) {
+        if (validacion(dni, nombre, telefono)) {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.telefono = telefono;
+        } else {
+            throw new RuntimeErrorException(null, "Error en la validacion");
+        }
+    }
+
+    // agregar validacion e irep con las 4 variables
+    public boolean validacion(int dni, String nombre, String telefono) {
+        if (dni <= 0 || nombre.isEmpty() || telefono.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
